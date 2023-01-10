b053e600d2b86cc9587a00999b346c66f12d62e7
Pavel Zdanovich <zdanovich.fti@gmail.com>
2023-01-10 11:05:47 +0300

    feat: add first screen slider

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Приветики-мурметики')
+const aboutBtn = document.querySelector('.first-screen__btn_about');
+const returnBtn = document.querySelector('.first-screen__btn_return');
+const firstScreenSlider = document.querySelector('.first-screen__slider');
+
+
+aboutBtn.addEventListener('click', moveSlide);
+returnBtn.addEventListener('click', returnSlide);
+
+function moveSlide() {
+   firstScreenSlider.style.marginLeft = '-110%';
+}
+
+function returnSlide() {
+   firstScreenSlider.style.marginLeft = '0%';
+}
