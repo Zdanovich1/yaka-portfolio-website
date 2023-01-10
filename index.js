f98b955fca248e3f48b7489da95fa3fbbc4d58b9
Pavel Zdanovich <zdanovich.fti@gmail.com>
2023-01-10 17:29:10 +0300

    feat: add portfolio section

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const aboutBtn = document.querySelector('.first-screen__btn_about');
+const linkAbout = document.getElementById('link__about');
 const returnBtn = document.querySelector('.first-screen__btn_return');
 const firstScreenSlider = document.querySelector('.first-screen__slider');
 
 
 aboutBtn.addEventListener('click', moveSlide);
+linkAbout.addEventListener('click', moveSlide);
 returnBtn.addEventListener('click', returnSlide);
 
 function moveSlide() {
