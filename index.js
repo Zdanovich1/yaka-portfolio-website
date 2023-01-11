e2b8b1985cbef31d8f33f7ae9206658e52a94841
Pavel Zdanovich <zdanovich.fti@gmail.com>
2023-01-11 12:41:48 +0300

    feat: add porfolio card functionality

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const aboutBtn = document.querySelector('.first-screen__btn_about');
 const linkAbout = document.getElementById('link__about');
 const returnBtn = document.querySelector('.first-screen__btn_return');
 const firstScreenSlider = document.querySelector('.first-screen__slider');
+const cardBtn = document.querySelectorAll('.card__button');
+const cardList = document.querySelectorAll('.card__list');
+const cardBlob = document.querySelectorAll('.card__blob');
+
+console.log(cardBtn)
 
 
 aboutBtn.addEventListener('click', moveSlide);
 linkAbout.addEventListener('click', moveSlide);
 returnBtn.addEventListener('click', returnSlide);
+
+
+cardBtn.forEach((el, index) => {
+   el.addEventListener('click', function (e) {
+      cardBlob[index].classList.add('big');
+      setTimeout(showList, 400, index);
+   })
+})
+
+
+/*helpers*/
+
+
+function showList(index) {
+   cardList[index].style.display = 'flex';
+}
 
 function moveSlide() {
    firstScreenSlider.style.marginLeft = '-110%';
